b1cd39dfcff1076c41a976a88322580c192bd7a8
jbruns <jbruns@live.com>
2025-02-17 13:07:46 -0800

    feat: automatic SOPS decryption on checkout

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ IFS=$'\n\t'
 PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
 DOCKER_VERSION="v27.1.2"
+SOPS_VERSION="v3.8.1"
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_VERSION"
+/usr/bin/env bash ./build/download_sops_binary.sh "$PLATFORM" "$ARCH" "$SOPS_VERSION"
